59ba0a5977c24fb7e9e009a325402b452b4a112e
mbutera11 <michael.butera11@gmail.com>
2018-02-16 14:14:30 -0600

    Start of Lab 4

--- Lab4/app/src/main/java/lab4/android/wku/edu/autopurchase/MainActivity.java
+++ Lab4/app/src/main/java/lab4/android/wku/edu/autopurchase/MainActivity.java
@@ -2,8 +2,13 @@
 
 package lab4.android.wku.edu.autopurchase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +17,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void sendMessage(View view) {
+        Intent in = new Intent(this, Report.class);
+        EditText et = findViewById(R.id.priceOfCar);
+        EditText et2 = findViewById(R.id.downPayment);
+        EditText et3 = findViewById(R.id.interestRate);
+
+        RadioGroup rGroup = findViewById(R.id.rgroup);
+        int selectedID = rGroup.getCheckedRadioButtonId();
+        RadioButton rButton = findViewById(selectedID);
+
+        int price = Integer.parseInt(et.getText().toString());
+        int downPay = Integer.parseInt(et2.getText().toString());
+        int rate = Integer.parseInt(et3.getText().toString());
+
+        in.putExtra("CarPrice", price);
+        in.putExtra("DownPayent", downPay);
+        in.putExtra("InterestRate", rate);
+        in.putExtra("LoanTerm", rButton.getText());
+
+        startActivity(in);
+    }
+
+
 }
