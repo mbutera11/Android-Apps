2c0cd5ac0cb5c06bb56941467eecfbd7e3960941
mbutera11 <michael.butera11@gmail.com>
2018-03-09 19:58:14 -0600

    Continue Lab 6

---
+++ ContactManager/app/src/main/java/lab6/android/wku/edu/contactmanager/DBHelper.java
@@ -0,0 +1,134 @@
+package lab6.android.wku.edu.contactmanager;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+/**
+ * Created by michaelbutera on 3/9/18.
+ */
+
+public class DBHelper extends SQLiteOpenHelper{
+
+    public static final String DATABASE_NAME = "ContactManager.db";
+
+    public static final String CONTACTS_TABLE_NAME = "contacts";
+    public static final String CONTACTS_COLUMN_ID = "id";
+    public static final String CONTACTS_COLUMN_NAME = "name";
+    public static final String CONTACTS_COLUMN_EMAIL = "email";
+    public static final String CONTACTS_COLUMN_STREET = "street";
+    public static final String CONTACTS_COLUMN_CITY = "place";
+    public static final String CONTACTS_COLUMN_PHONE = "phone";
+
+    public static final String USERS_TABLE_NAME = "users";
+    public static final String USERS_COLUMN_ID = "id";
+    public static final String USERS_COLUMN_USERNAME = "username";
+    public static final String USERS_COLUMN_PASSWORD = "password";
+
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // create contacts table
+        db.execSQL(
+                "create table contacts " +
+                        "(id integer primary key autoincrement, name text,phone text,email text, street text,place text)"
+        );
+
+        // create users table
+        db.execSQL(
+                "create table users " +
+                        "(id integer primary key, username text, password text)"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS contacts");
+        db.execSQL("DROP TABLE IF EXISTS users");
+        onCreate(db);
+    }
+
+    // insert contact to DB
+    public boolean insertContact(String name, String email, String street, String place, String phone) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("name", name);
+        cv.put("email", email);
+        cv.put("street", street);
+        cv.put("place", place);
+        cv.put("phone", phone);
+        db.insert("contacts", null, cv);
+
+        return true;
+    }
+
+    // insert user to DB
+    public boolean insertUser(String username, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("username", username);
+        cv.put("password", password);
+        db.insert("users", null, cv);
+
+        return true;
+    }
+
+    public ArrayList<Cursor> getData(String Query){
+        //get writable database
+        SQLiteDatabase sqlDB = this.getWritableDatabase();
+        String[] columns = new String[] { "message" };
+        //an array list of cursor to save two cursors one has results from the query
+        //other cursor stores error message if any errors are triggered
+        ArrayList<Cursor> alc = new ArrayList<Cursor>(2);
+        MatrixCursor Cursor2= new MatrixCursor(columns);
+        alc.add(null);
+        alc.add(null);
+
+        try{
+            String maxQuery = Query ;
+            //execute the query results will be save in Cursor c
+            Cursor c = sqlDB.rawQuery(maxQuery, null);
+
+            //add value to cursor2
+            Cursor2.addRow(new Object[] { "Success" });
+
+            alc.set(1,Cursor2);
+            if (null != c && c.getCount() > 0) {
+
+                alc.set(0,c);
+                c.moveToFirst();
+
+                return alc ;
+            }
+            return alc;
+        } catch(SQLException sqlEx){
+            Log.d("printing exception", sqlEx.getMessage());
+            //if any exceptions are triggered save the error message to cursor an return the arraylist
+            Cursor2.addRow(new Object[] { ""+sqlEx.getMessage() });
+            alc.set(1,Cursor2);
+            return alc;
+        } catch(Exception ex){
+            Log.d("printing exception", ex.getMessage());
+
+            //if any exceptions are triggered save the error message to cursor an return the arraylist
+            Cursor2.addRow(new Object[] { ""+ex.getMessage() });
+            alc.set(1,Cursor2);
+            return alc;
+        }
+    }
+
+
+
+}
--- ContactManager/app/src/main/java/lab6/android/wku/edu/contactmanager/RegisterActivity.java
+++ ContactManager/app/src/main/java/lab6/android/wku/edu/contactmanager/RegisterActivity.java
@@ -1,9 +1,20 @@
 package lab6.android.wku.edu.contactmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    protected EditText userName;
+    protected EditText password;
+    protected EditText confirmPass;
+    protected Button b;
+    private DBHelper db;
+    protected Button b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,7 +22,51 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         getSupportActionBar().setTitle("Register New Account");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        db = new DBHelper(this);
+        Button b = findViewById(R.id.register);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                userName = findViewById(R.id.username);
+                password = findViewById(R.id.password);
+                confirmPass = findViewById(R.id.confirmpass);
+
+                // if password and confirm password match, insert user
+                if(password.getText().toString().equals(confirmPass.getText().toString())) {
+                    db.insertUser(userName.getText().toString(), password.getText().toString());
+//                    Intent in =  new Intent(RegisterActivity.this, Contacts.class);
+//                    startActivity(in);
+                } else {
+
+                }
+
+            }
+        });
+
+        b2 = findViewById(R.id.button1);
+
+        b2.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              Intent dbmanager = new Intent(RegisterActivity.this,AndroidDatabaseManager.class);
+              startActivity(dbmanager);
+          }
+        });
+
+
+
     }
+
+
+
+
+
+
+
+
+
 
     @Override
     public boolean onSupportNavigateUp() {
