69e5bfea58d073d6480e9e9f1bc385b196a623d2
mbutera11 <michael.butera11@gmail.com>
2018-04-06 18:59:43 -0500

    Finish Lab 8

--- WeatherMB/app/src/main/java/lab8/android/wku/edu/weathermb/MainActivity.java
+++ WeatherMB/app/src/main/java/lab8/android/wku/edu/weathermb/MainActivity.java
@@ -1,19 +1,22 @@
 package lab8.android.wku.edu.weathermb;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONException;
@@ -36,25 +39,33 @@ public class MainActivity  extends FragmentActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
         location = findViewById(R.id.location);
         go = findViewById(R.id.go);
         temperature = findViewById(R.id.temp);
 
+        // execute Async task on button click
         go.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new WeatherData().execute(location.getText().toString());
+
+                // hide keyboard, source: https://stackoverflow.com/questions/3400028/close-virtual-keyboard-on-button-press
+                InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+                // if input is empty, toast message
+                // else execute ASYNC task
+                if(location.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "Enter a location", Toast.LENGTH_LONG).show();
+                } else {
+                    new WeatherData().execute(location.getText().toString());
+                }
             }
         });
-
-
-
-
     }
 
     private class WeatherData extends AsyncTask<String, Integer, JSONObject> {
@@ -62,28 +73,32 @@ public class MainActivity  extends FragmentActivity implements OnMapReadyCallbac
         @Override
         protected JSONObject doInBackground(String... strings) {
 
+            // location the user typed in
             String location = strings[0];
             try {
+                // url that has the location and API key
                 URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=1c7af89f87bc41ecd1c3ebfe3f117ca8");
+                // connect to URL
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                // read URL data
                 BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
                 StringBuffer jsonOutput = new StringBuffer(1024);
-                String temp = "";
+                String temp;
 
+                // while there is info to be read from the url, add to string buffer
                 while((temp = br.readLine()) != null) {
                     jsonOutput.append(temp).append("\n");
                 }
                 br.close();
 
+                // convert string buffer to string, create JSON object and return
                 JSONObject jsonObject = new JSONObject(jsonOutput.toString());
                 return jsonObject;
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
-
-
             return null;
         }
 
@@ -91,21 +106,41 @@ public class MainActivity  extends FragmentActivity implements OnMapReadyCallbac
         protected void onPostExecute(JSONObject jsonObject) {
             try {
 
-                JSONObject latlng = jsonObject.getJSONObject("coord");
-                double lat = latlng.getDouble("lat");
-                double lon = latlng.getDouble("lon");
-                coordinates = new LatLng(lat, lon);
+                // if the jsonObject is null, the address was not valid, toast message
+                // else, continue to get temperature and set map
+                if (jsonObject == null) {
+                    Toast.makeText(getApplicationContext(), "Enter a valid location", Toast.LENGTH_LONG).show();
+                } else {
+                    // read latitude and longitude from JSON
+                    JSONObject latlng = jsonObject.getJSONObject("coord");
+                    double lat = latlng.getDouble("lat");
+                    double lon = latlng.getDouble("lon");
 
-                mMap.addMarker(new MarkerOptions().position(coordinates).title(location.getText().toString()));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(coordinates));
+                    // initialize LatLng object with the latitude and longitude from JSON
+                    coordinates = new LatLng(lat, lon);
 
+                    // create marker with coordinates and text title
+                    Marker marker = mMap.addMarker(new MarkerOptions().position(coordinates).title(location.getText().toString()));
+                    marker.showInfoWindow();
 
-                JSONObject main = jsonObject.getJSONObject("main");
-                double temp = main.getDouble("temp");
-                temperature.setText(Double.toString(temp));
+                    // moves camera to the location and sets zoom to 15
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 10));
 
+                    // JSON to get temperature
+                    JSONObject main = jsonObject.getJSONObject("main");
 
-            } catch (JSONException e) {
+                    // temperature in kelvin, celsius, and fahrenheit
+                    double tempK = main.getDouble("temp");
+                    double tempC = tempK - 273.15;
+                    double tempF = tempC * 1.8 + 32;
+
+                    // format to two decimal places
+                    double tempToShow = Math.floor(tempF * 100) / 100;
+                    // change text on screen to temperature
+                    temperature.setText(Double.toString(tempToShow) + "\u00b0 F");
+
+                }
+            } catch(JSONException e){
                 e.printStackTrace();
             }
         }
@@ -115,10 +150,5 @@ public class MainActivity  extends FragmentActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
